1f82be63a3a8f47200256d3209abe8291ce832dd
EithanSanchezFilipe <150446486+EithanSanchezFilipe@users.noreply.github.com>
2025-02-12 13:24:13 +0100

    feat : custom error messages

--- start/validator.ts
+++ start/validator.ts
@@ -1,14 +1,26 @@
 import vine, { SimpleMessagesProvider } from '@vinejs/vine'
 
 export const customErrorMessage = new SimpleMessagesProvider({
-  // Applicable for all fields
-  'required': 'The {{ field }} field is required',
-  'string': 'The value of {{ field }} field must be a string',
-  'email': 'The value is not a valid email address',
+  // Généralement pour tous les champs
+  'email': "Veuillez respecter le format d'email valide.",
+  'username': "Veuillez spécifier un nom d'utilisateur.",
+  'password': 'Veuillez spécifier votre mot de passe.',
+  'confirmPassword': 'Veuillez confirmer votre mot de passe.',
 
-  // Error message for the username field
-  'username.required': 'Please choose a username for your account',
+  // Erreurs pour les champs requis
+  'username.required': "Veuillez spécifier votre nom d'utilisateur.",
+  'password.required': 'Veuillez spécifier votre mot de passe.',
+  'email.required': "L'email est requis.",
+  'confirmPassword.required': 'La confirmation du mot de passe est requise.',
 
-  //Error message for the password field
+  // Erreurs pour la validation de format
+  'email.email': "L'email doit être dans un format valide, comme exemple@domaine.com.",
+  'password.minLength': 'Le mot de passe doit contenir au moins 8 caractères.',
+
+  // Erreurs pour les champs de mot de passe confirmés
   'password.confirmed': 'Les mots de passe ne correspondent pas.',
+
+  // Erreurs pour les validations uniques (comme l\'email ou le nom d\'utilisateur déjà pris)
+  'username.unique': "Ce nom d'utilisateur est déjà pris.",
+  'email.unique': 'Cet email est déjà associé à un compte.',
 })
